31b0213bdf385273df9b613bc1485ec6663127d5
DESKTOP-67BT1F3\Panda <chrisrberg03@gmail.com>
2024-02-20 01:24:57 +0100

    added images from api to dom

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
+const movieContainer = document.getElementById("movieContainer");
+const movieAllArray = [];
+const movieFilteredArray = [];
+
+// fetch the data and parse it to json
 async function fetchData() {
     const reponse = await fetch("https://api.noroff.dev/api/v1/square-eyes");
     const result = await reponse.json();
-    console.log(result);
-    return result;
+
+    movieAllArray.push(...result);
+
+    for (const movie of movieAllArray) {
+        displayMovie(movie);
+    }
+    console.log(movieAllArray);
 }
 
 fetchData();
+
+function displayMovie(movie) {
+    const movieList = document.createElement("li");
+    const movieImg = document.createElement("img");
+    movieImg.src = movie.image;
+    movieList.appendChild(movieImg);
+    movieContainer.appendChild(movieList);
+}
